ba7772d39d7d516e69d2278d6867586f70275f41
LightSirius <54878053+LightSirius@users.noreply.github.com>
2023-09-22 12:00:09 +0900

    Add userauth entity

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,10 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { UserAuth } from './entities/user-auth.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, UserAuth])],
   controllers: [UserController],
   providers: [UserService],
 })
