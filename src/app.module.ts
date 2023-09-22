d477228487b80aa381d6c8fea1eae3cead87737a
LightSirius <54878053+LightSirius@users.noreply.github.com>
2023-09-22 14:51:24 +0900

    Add auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
     UserModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { UserService } from '../user/user.service';
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly userService: UserService) {}
+
+  async validateUser(auth_id: string, auth_password: string): Promise<any> {
+    const user = await this.userService.authFindUser(auth_id);
+    if (user && user.userAuth.auth_password === auth_password) {
+      const { userAuth, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+}
