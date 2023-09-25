9f0a4723c1cc7a3afcff5c0b9c0304b9a5cb2a0d
LightSirius <54878053+LightSirius@users.noreply.github.com>
2023-09-25 14:55:25 +0900

    Add swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swagger_config = new DocumentBuilder()
+    .setTitle('T2 Project API')
+    .setDescription('T2 Backend API List')
+    .setVersion('0.0.1')
+    .build();
+  const swagger_document = SwaggerModule.createDocument(app, swagger_config);
+  SwaggerModule.setup('api-list', app, swagger_document);
+
   await app.listen(3000);
 }
 bootstrap();
