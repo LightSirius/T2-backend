a2cabbf0a58e74f03a5e5c250dbec6ffe1e48606
LightSirius <54878053+LightSirius@users.noreply.github.com>
2023-09-25 11:30:16 +0900

    Add jwt strategy

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,18 @@
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: 'TEST',
+    });
+  }
+
+  async validate(payload: any) {
+    return { uuid: payload.uuid };
+  }
+}
