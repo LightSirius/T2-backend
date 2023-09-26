766a8e7fcc764c09ea7b756301e6780a067ca42b
LightSirius <54878053+LightSirius@users.noreply.github.com>
2023-09-26 14:06:55 +0900

    Add dto validate

--- src/user/dto/create-user-auth.dto.ts
+++ src/user/dto/create-user-auth.dto.ts
@@ -1,8 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
 
 export class CreateUserAuthDto {
   @ApiProperty({ description: 'auth_id' })
+  @IsString()
   auth_id: string;
   @ApiProperty({ description: 'auth_password' })
+  @IsString()
   auth_password: string;
 }
