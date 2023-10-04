5ee268b22515014b35619cd3070912855dc9b7a0
LightSirius <54878053+LightSirius@users.noreply.github.com>
2023-10-04 18:04:58 +0900

    Add show controller

--- src/show/show.module.ts
+++ src/show/show.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { ShowController } from './show.controller';
 
-@Module({})
+@Module({
+  controllers: [ShowController]
+})
 export class ShowModule {}
