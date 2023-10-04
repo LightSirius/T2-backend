351c483d0b08aa0162825b6bc538503cf391c7e4
LightSirius <54878053+LightSirius@users.noreply.github.com>
2023-10-04 18:05:39 +0900

    Add show service

--- src/show/show.module.ts
+++ src/show/show.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ShowController } from './show.controller';
+import { ShowService } from './show.service';
 
 @Module({
-  controllers: [ShowController]
+  controllers: [ShowController],
+  providers: [ShowService]
 })
 export class ShowModule {}
