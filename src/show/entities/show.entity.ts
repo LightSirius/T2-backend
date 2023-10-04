e22a9e42516b07b5fac9c148080c148a92df4c65
LightSirius <54878053+LightSirius@users.noreply.github.com>
2023-10-04 18:45:50 +0900

    Update show.entity.ts

--- src/show/entities/show.entity.ts
+++ src/show/entities/show.entity.ts
@@ -1,1 +1,38 @@
-export class Show {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Show {
+  constructor(show: Partial<Show>) {
+    Object.assign(this, show);
+  }
+
+  @PrimaryGeneratedColumn()
+  show_id: number;
+
+  @Column()
+  show_name: string;
+
+  @Column()
+  show_details: string;
+
+  @Column()
+  show_description: string;
+
+  @Column()
+  show_start_date: Date;
+
+  @CreateDateColumn()
+  create_date: Date;
+
+  @UpdateDateColumn()
+  update_date: Date;
+
+  @Column()
+  alter_user: string;
+}
