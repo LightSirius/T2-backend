e4d188bc490aae260466bcb06c431e95b75fa425
LightSirius <54878053+LightSirius@users.noreply.github.com>
2023-10-05 18:31:21 +0900

    Add create dto vlidate

--- src/show/dto/create-show.dto.ts
+++ src/show/dto/create-show.dto.ts
@@ -1,1 +1,22 @@
-export class CreateShowDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsDate, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class CreateShowDto {
+  @ApiProperty({ description: 'show_name' })
+  @IsString()
+  show_name: string;
+  @ApiProperty({ description: 'show_details' })
+  @IsString()
+  show_details: string;
+  @ApiProperty({ description: 'show_description' })
+  @IsString()
+  show_description: string;
+  @ApiProperty({ description: 'show_start_date' })
+  @IsDate()
+  @Type(() => Date)
+  show_start_date: Date;
+  @ApiProperty({ description: 'alter_user' })
+  @IsString()
+  alter_user: string;
+}
