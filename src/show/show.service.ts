1e0d067418c3dcb02bc5fc0991036793691b4548
LightSirius <54878053+LightSirius@users.noreply.github.com>
2023-10-05 18:31:52 +0900

    Add show entity handle service

--- src/show/show.service.ts
+++ src/show/show.service.ts
@@ -1,23 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { CreateShowDto } from './dto/create-show.dto';
 import { UpdateShowDto } from './dto/update-show.dto';
+import { Show } from './entities/show.entity';
+import { EntityManager, Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class ShowService {
-  create(createShowDto: CreateShowDto) {
-    return 'This action adds a new show';
+  constructor(
+    @InjectRepository(Show)
+    private showRepository: Repository<Show>,
+    private readonly entityManager: EntityManager,
+  ) {}
+  async create(createShowDto: CreateShowDto) {
+    const show = new Show(createShowDto);
+    await this.entityManager.save(show);
   }
 
-  findAll() {
-    return `This action returns all show`;
+  async findAll(): Promise<Show[]> {
+    return this.showRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} show`;
+  async findOne(show_id: number) {
+    return this.showRepository.findOneBy({ show_id });
   }
 
-  update(id: number, updateShowDto: UpdateShowDto) {
-    return `This action updates a #${id} show`;
+  async update(show_id: number, updateShowDto: UpdateShowDto) {
+    const show = await this.findOne(show_id);
+    return await this.showRepository.save({ ...show, ...updateShowDto });
   }
 
   remove(id: number) {
