f8dcf7fb6dbec9454b2464a7e675821df417b81e
LightSirius <54878053+LightSirius@users.noreply.github.com>
2023-10-05 18:30:22 +0900

    Init show entity

--- src/show/show.module.ts
+++ src/show/show.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ShowController } from './show.controller';
 import { ShowService } from './show.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Show } from './entities/show.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Show])],
   controllers: [ShowController],
   providers: [ShowService],
 })
