ba1ed2c87a41e19b22eff2eed591ea54c54bb4fd
LightSirius <54878053+LightSirius@users.noreply.github.com>
2023-10-06 10:11:29 +0900

    Update show.controller.ts

--- src/show/show.controller.ts
+++ src/show/show.controller.ts
@@ -11,24 +11,25 @@ import { ShowService } from './show.service';
 import { CreateShowDto } from './dto/create-show.dto';
 import { UpdateShowDto } from './dto/update-show.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { Show } from './entities/show.entity';
 
 @ApiTags('Show API')
 @Controller('show')
 export class ShowController {
   constructor(private readonly showService: ShowService) {}
 
-  @Post()
+  @Post('create')
   create(@Body() createShowDto: CreateShowDto) {
     return this.showService.create(createShowDto);
   }
 
   @Get()
-  findAll() {
+  findAll(): Promise<Show[]> {
     return this.showService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id') id: string): Promise<Show> {
     return this.showService.findOne(+id);
   }
 
