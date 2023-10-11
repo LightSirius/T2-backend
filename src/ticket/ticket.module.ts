e1068af9ed54746a9a4d01861b8fe510985406c1
LightSirius <54878053+LightSirius@users.noreply.github.com>
2023-10-11 18:45:54 +0900

    Add imports redis module

--- src/ticket/ticket.module.ts
+++ src/ticket/ticket.module.ts
@@ -3,9 +3,10 @@ import { TicketService } from './ticket.service';
 import { TicketController } from './ticket.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Ticket } from './entities/ticket.entity';
+import { RedisModule } from '../redis/redis.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Ticket])],
+  imports: [TypeOrmModule.forFeature([Ticket]), RedisModule],
   controllers: [TicketController],
   providers: [TicketService],
   exports: [TicketService],
