22c9dbf19fc7816095960710d104531b2325ddb6
LightSirius <54878053+LightSirius@users.noreply.github.com>
2023-10-11 18:46:56 +0900

    Add check test function

--- src/ticket/ticket.controller.ts
+++ src/ticket/ticket.controller.ts
@@ -1,8 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { TicketService } from './ticket.service';
 import { CreateTicketDto } from './dto/create-ticket.dto';
 import { UpdateTicketDto } from './dto/update-ticket.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Ticket API')
 @Controller('ticket')
 export class TicketController {
   constructor(private readonly ticketService: TicketService) {}
@@ -30,5 +40,15 @@ export class TicketController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.ticketService.remove(+id);
+  }
+
+  @Post('/tget')
+  t_get(@Body() req: { key: string }) {
+    return this.ticketService.redis_test_get(req.key);
+  }
+
+  @Post('/tset')
+  t_set(@Body() req: { key: string; val: string }) {
+    return this.ticketService.redis_test_set(req.key, req.val);
   }
 }
--- src/ticket/ticket.service.ts
+++ src/ticket/ticket.service.ts
@@ -1,13 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateTicketDto } from './dto/create-ticket.dto';
 import { UpdateTicketDto } from './dto/update-ticket.dto';
 import { EntityManager, Repository } from 'typeorm';
 import { Ticket } from './entities/ticket.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { RedisClientType } from 'redis';
 
 @Injectable()
 export class TicketService {
   constructor(
+    @Inject('REDIS_CLIENT')
+    private readonly redis: RedisClientType,
     @InjectRepository(Ticket)
     private ticketRepository: Repository<Ticket>,
     private readonly entityManager: EntityManager,
@@ -32,5 +35,15 @@ export class TicketService {
 
   remove(id: number) {
     return `This action removes a #${id} ticket`;
+  }
+
+  reserve_seat() {}
+
+  redis_test_get(key: string) {
+    return this.redis.get(key);
+  }
+
+  redis_test_set(key: string, val: any) {
+    return this.redis.set(key, val);
   }
 }
