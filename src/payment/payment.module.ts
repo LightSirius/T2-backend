3b1e80b5fd4924759961b735b192ac42402b7985
LightSirius <54878053+LightSirius@users.noreply.github.com>
2023-10-13 15:40:21 +0900

    Update payment module import entites

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { PaymentController } from './payment.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PaymentList } from './entities/payment-list.entity';
+import { PaymentPortone } from './entities/payment-portone.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([PaymentList, PaymentPortone])],
   controllers: [PaymentController],
   providers: [PaymentService],
 })
