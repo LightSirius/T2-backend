f6ea4b1fda09def83c696824024e3ed250a7d294
LightSirius <54878053+LightSirius@users.noreply.github.com>
2023-10-17 18:43:52 +0900

    Update board api

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -1,13 +1,23 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { BoardService } from './board.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { UpdateBoardDto } from './dto/update-board.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Board API')
 @Controller('board')
 export class BoardController {
   constructor(private readonly boardService: BoardService) {}
 
-  @Post()
+  @Post('create')
   create(@Body() createBoardDto: CreateBoardDto) {
     return this.boardService.create(createBoardDto);
   }
