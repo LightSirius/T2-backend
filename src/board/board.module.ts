dea6a25315f19e9ec129acce444f5d13274b4284
LightSirius <54878053+LightSirius@users.noreply.github.com>
2023-10-25 16:08:33 +0900

    Import redis module

--- src/board/board.module.ts
+++ src/board/board.module.ts
@@ -3,9 +3,10 @@ import { BoardService } from './board.service';
 import { BoardController } from './board.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Board } from './entities/board.entity';
+import { RedisModule } from '../redis/redis.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Board])],
+  imports: [TypeOrmModule.forFeature([Board]), RedisModule],
   controllers: [BoardController],
   providers: [BoardService],
   exports: [BoardService],
