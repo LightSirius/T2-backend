77b996ccaf83f87b3ff573fff3231c7425899bd4
LightSirius <54878053+LightSirius@users.noreply.github.com>
2023-11-06 14:51:05 +0900

    Update board-detail.dto.ts

--- src/board/dto/board-detail.dto.ts
+++ src/board/dto/board-detail.dto.ts
@@ -1,6 +1,17 @@
+import { BoardType } from '../entities/board.entity';
+
+class NearBoardList {
+  board_id: number;
+  board_title: string;
+  create_date: Date;
+}
+
 export class BoardDetailDto {
+  board_id: number;
+  board_type: BoardType;
   board_title: string;
   board_contents: string;
   user_name: string;
   update_date: Date;
+  near_board_list: NearBoardList;
 }
