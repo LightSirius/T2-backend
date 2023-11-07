3a8f4e1d97826865912ac9bb0d521838b2010e6f
LightSirius <54878053+LightSirius@users.noreply.github.com>
2023-11-07 11:12:24 +0900

    Add comment entity

---
+++ src/comment/entities/comment.entity.ts
@@ -0,0 +1,35 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Comment {
+  constructor(comment: Comment) {
+    Object.assign(this, comment);
+  }
+
+  @PrimaryGeneratedColumn()
+  comment_id: number;
+
+  @Column()
+  board_id: number;
+
+  @Column()
+  comment_reply_id: number;
+
+  @Column({ type: 'uuid' })
+  user_uuid: string;
+
+  @Column()
+  comment_contents: string;
+
+  @CreateDateColumn()
+  create_date: Date;
+
+  @UpdateDateColumn()
+  update_date: Date;
+}
