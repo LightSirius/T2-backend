afc5b243091525ef2e30c860fd3abf4366be5387
LightSirius <54878053+LightSirius@users.noreply.github.com>
2023-11-08 15:31:06 +0900

    Add comment list function

--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -4,6 +4,7 @@ import { UpdateCommentDto } from './dto/update-comment.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Comment } from './entities/comment.entity';
 import { EntityManager, Repository } from 'typeorm';
+import { User } from '../user/entities/user.entity';
 
 @Injectable()
 export class CommentService {
@@ -36,5 +37,35 @@ export class CommentService {
 
   remove(id: number) {
     return `This action removes a #${id} comment`;
+  }
+
+  async comment_list(board_id: number, page: number) {
+    const sql_limit = 20;
+    const sql_page = page - 1 > 0 ? page - 1 : 0;
+    const sql_offset = sql_page * sql_limit;
+
+    return {
+      total_page: Math.ceil(
+        (await this.commentRepository.count({
+          where: {
+            board_id: board_id,
+          },
+        })) / sql_limit,
+      ),
+      comment_list: await this.commentRepository
+        .createQueryBuilder('comment')
+        .leftJoinAndSelect(User, 'user', 'comment.user_uuid = user.user_uuid')
+        .select([
+          'comment.comment_id AS comment_id',
+          'comment.comment_reply_id AS comment_reply_id',
+          'comment.comment_contents AS comment_contents',
+          'user.user_name AS user_name',
+        ])
+        .where('comment.board_id = :board_id', { board_id: board_id })
+        .orderBy('comment.comment_sort_idx')
+        .limit(sql_limit)
+        .offset(sql_offset)
+        .getRawMany(),
+    };
   }
 }
