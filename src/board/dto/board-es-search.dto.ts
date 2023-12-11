56c6d296aeb98328dcac57d9b2478a9223e56ceb
LightSirius <54878053+LightSirius@users.noreply.github.com>
2023-12-11 18:45:45 +0900

    Add es search dto description

--- src/board/dto/board-es-search.dto.ts
+++ src/board/dto/board-es-search.dto.ts
@@ -5,16 +5,19 @@ export class BoardEsSearchDto {
   @ApiProperty({ description: 'search_string' })
   @IsString()
   search_string: string;
-  @ApiProperty({ description: 'sort_type' })
-  @IsNumber()
-  sort_type: number;
-  @ApiProperty({ description: 'search_type' })
-  @IsNumber()
-  search_type: number;
-  @ApiProperty({ description: 'board_type' })
-  @IsNumber()
-  board_type: number;
   @ApiProperty({ description: 'search_from' })
   @IsNumber()
   search_from: number;
+  @ApiProperty({
+    description:
+      'search_type<br>1: board title<br>2: board contents<br>3: user name',
+  })
+  @IsNumber()
+  search_type: number;
+  @ApiProperty({ description: 'sort_type<br>0: score<br>1: newest' })
+  @IsNumber()
+  sort_type: number;
+  @ApiProperty({ description: 'board_type' })
+  @IsNumber()
+  board_type: number;
 }
